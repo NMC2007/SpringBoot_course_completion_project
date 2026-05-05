99ec38e9fea0a61995c30f88caf687f32c1548c9
NMC2007 <nmc281207@gmail.com>
2026-05-05 19:33:59 +0700

    Create Project

---
+++ src/main/java/com/example/completion_project/CompletionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.completion_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompletionProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/completion_project/CompletionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.completion_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletionProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
